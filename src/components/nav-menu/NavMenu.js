b4479a5e85eb2213cfaaa0b8ebc4dc125dc1fbf0
喻博文 <747137830@qq.com>
2018-05-18 10:05:58 +0800

    基础架构
    我也没想到就这样就能解决这个问题。。。。

--- src/components/nav-menu/NavMenu.js
+++ src/components/nav-menu/NavMenu.js
@@ -21,7 +21,7 @@ export default {
             this.$store.commit('setRouterName',routerName)            
         },
         getData(){
-            let url = this.$route.path
+            let url = this.$route.path.replace('Detail','')
             console.log(url);
             
             setTimeout(()=>{
